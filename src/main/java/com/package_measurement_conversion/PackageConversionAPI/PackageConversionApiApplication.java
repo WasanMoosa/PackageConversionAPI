991647f4aa56ea877bb1b7c5853cd7bd6e8e43a8
WasanMoosa <wasssoon@gmail.com>
2023-05-15 10:43:35 +0400

    First push

---
+++ src/main/java/com/package_measurement_conversion/PackageConversionAPI/PackageConversionApiApplication.java
@@ -0,0 +1,13 @@
+package com.package_measurement_conversion.PackageConversionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageConversionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageConversionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/package_measurement_conversion/PackageConversionAPI/PackageConversionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.package_measurement_conversion.PackageConversionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageConversionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
