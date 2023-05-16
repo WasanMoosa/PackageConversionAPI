dc1e62ccc1ded8ab91ab6943d0f8f674be5ff234
WasanMoosa <wasssoon@gmail.com>
2023-05-16 11:35:18 +0400

    Finish the conversion logic in main

---
+++ src/main/java/com/package_measurement_conversion/PackageConversionAPI/Service/ConversionMeasurementService.java
@@ -0,0 +1,4 @@
+package com.package_measurement_conversion.PackageConversionAPI.Service;
+
+public class ConversionMeasurementService {
+}
--- src/main/java/com/package_measurement_conversion/PackageConversionAPI/controller/mainController.java
+++ src/main/java/com/package_measurement_conversion/PackageConversionAPI/controller/mainController.java
@@ -1,5 +1,6 @@
 package com.package_measurement_conversion.PackageConversionAPI.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,20 @@ import java.util.HashMap;
 @RequestMapping
 public class mainController {
   @GetMapping("/convert-measurements")
-  public String convertMeasurements(@RequestParam(value = "input", defaultValue = "no measurement") String input) {
-    int[] measurements = {1, 2, 1}; // replace with your own logic for converting measurements
-    String body = Arrays.toString(measurements);
-    if (input.equals("no measurement") ){
-      input = "coco";
-    }
+  public HashMap<String, Object> convertMeasurements(@RequestParam(value = "input", defaultValue = "no measurement") String input) {
+    HashMap<String, Object> response = new HashMap<>();
 
-    return input;
+    response.put("body", input);
+
+    HttpStatus httpStatus = HttpStatus.OK;
+
+    // check if response is not OK and set the status accordingly
+    if (httpStatus.isError()) {
+      response.put("status", httpStatus.value());
+    } else {
+      response.put("status", "200 OK");
+    }
+    return response;
   }
 }
 
