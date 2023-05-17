023fbb5637b1dba7362df381686079f9ee5471ff
WasanMoosa <wasssoon@gmail.com>
2023-05-17 13:28:41 +0400

    Adding comment

--- src/main/java/com/package_measurement_conversion/PackageConversionAPI/controller/MainController.java
+++ src/main/java/com/package_measurement_conversion/PackageConversionAPI/controller/MainController.java
@@ -11,29 +11,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
+
 @RestController
-@RequestMapping
-public class mainController {
-    @Autowired
-    ConversionMeasurementService conversionMeasurementService;
+@RequestMapping("/api")
+public class MainController {
 
     @Autowired
-    InputValidationService inputValidationService;
+    private ConversionMeasurementService conversionMeasurementService;
 
+    @Autowired
+    private InputValidationService inputValidationService;
+
+    /**
+     * Convert measurements based on the provided input.
+     *
+     * @param input The input string for measurements conversion.
+     * @return The converted measurements or an error message.
+     */
     @GetMapping("/convert-measurements")
     public Object convertMeasurements(@RequestParam(value = "input") String input) {
         List<Integer> numList = new ArrayList<>();
+
         if (inputValidationService.checkInputValidation(input)) {
+
+            // If the input is valid, apply the special logic for measurement conversion
             return conversionMeasurementService.applySpecialLogic(input);
         } else if (inputValidationService.isEmptyString(input)) {
+
+            // If the input is an empty string, return an empty list
             return numList;
         } else {
-            return "You are typing something wrong!!" + "\n You can only type letters and under score";
-        }
 
+            // If the input is invalid, return an error message
+            return "You are typing something wrong!!" + "\nYou can only type letters and underscores";
+        }
     }
 }
-
